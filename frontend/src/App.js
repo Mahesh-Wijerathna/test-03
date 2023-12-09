1876948a013e6ac37607e65240c27a9423291bee
Mahesh_Wijerathna <maheshtharanga49@gmail.com>
2023-12-09 15:02:14 +0530

    jhyghjg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import Locations from './Components/Locations'
+import MedicalCenter from './Components/signupMC';
+import Home from './Components/Home';
+import Signup from './Components/Signup';
 
-function App() {
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { useSelector } from "react-redux";
+import Signupscreen from './Components/signupscreen';
+
+const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload 0k.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div className="app">
+
+          
+
+              <Router>
+                  <Routes>
+
+                      <Route path="/" element={<Home/>}/>,
+                      <Route path="/signup/tourist" element={<Signup/>}/>,
+                      <Route path="/signup/medical_center" element={<MedicalCenter/>}/>,
+                      <Route path="/create_locations" element={<Locations/>}/>,
+                  </Routes>
+              </Router>
+          
+      </div>
+
   );
-}
+};
 
 export default App;
