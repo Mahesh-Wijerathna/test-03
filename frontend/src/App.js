2d7593a9fbfbb81652c0a43a06cd6f047b000535
Mahesh_Wijerathna <maheshtharanga49@gmail.com>
2023-12-09 15:56:13 +0530

    tytytu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,23 +5,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload 0k.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="app">
+
+        
+
+            <Router>
+                <Routes>
+
+                    <Route path="/" element={<Home/>}/>,
+                    <Route path="/signup/tourist" element={<Signup/>}/>,
+                    <Route path="/signup/medical_center" element={<MedicalCenter/>}/>,
+                    <Route path="/create_locations" element={<Locations/>}/>,
+                </Routes>
+            </Router>
+        
     </div>
-  );
+
+);
 }
 
 export default App;
